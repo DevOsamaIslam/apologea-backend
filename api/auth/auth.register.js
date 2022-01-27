47a274ba51d215bef90b4666db3a70929c922d90
Osama Islam <osamasamarrai@gmail.com>
2022-01-27 16:39:27 +0200

    fix mismatched keys

--- api/auth/auth.register.js
+++ api/auth/auth.register.js
@@ -1,12 +1,15 @@
 import User from '../../models/User.js'
-import { asyncHandler } from '../../config/utils.js'
+import strings from '../../config/strings.js'
 
 export default async (req, res) => {
 	User.create({
-		name: req.body.name,
+		username: req.body.username,
 		email: req.body.email,
 		password: req.body.password
 	})
-		.then(data => res.json(data))
+		.then(data => res.json({
+			account: data._id,
+			message: strings.successKey
+		}))
 		.catch(error => res.json(error))
 }
