2d00e1c051534f122d471374c6bae5a43fecaa8c
Osama Islam <osamasamarrai@gmail.com>
2022-01-27 16:37:31 +0200

    remove react implementaton fom server

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,32 @@
 module.exports = {
 	'env': {
-		'browser': true,
 		'es2021': true,
 		'node': true
 	},
 	'extends': [
 		'eslint:recommended',
-		'plugin:react/recommended'
 	],
 	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
-		},
 		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'react'
-	],
-	'rules': {
-		'no-unused-vars': ['warn'],
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
+		'sourceType': 'module',
+		'rules': {
+			'no-unused-vars': ['warn'],
+			'indent': [
+				'error',
+				'tab'
+			],
+			'linebreak-style': [
+				'error',
+				'unix'
+			],
+			'quotes': [
+				'error',
+				'single'
+			],
+			'semi': [
+				'error',
+				'never'
+			]
+		}
 	}
 }
