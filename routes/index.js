8b80dd4ff880cecf3c33b7a553a4af3adad9ef6d
Osama Islam <osamasamarrai@gmail.com>
2022-01-28 19:32:05 +0200

    adding the new routes to the directory

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ import express from 'express'
 import auth from './auth/index.js'
 import user from './profile/index.js'
 import blog from './blog/index.js'
+import actions from './actions/index.js'
+import feed from './feed.js'
+import { authed } from '../lib/utils.js'
 
 const rt = express.Router()
 
 rt.use('/auth', auth)
-rt.use('/user', user)
-rt.use('/blog', blog)
+rt.use('/feed', feed)
+rt.use('/user', authed, user)
+rt.use('/blog', authed, blog)
+rt.use('/actions', authed, actions)
 
 
 export default rt
