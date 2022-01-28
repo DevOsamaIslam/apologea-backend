b9112e231c2d21cd6cbcbe448de7406af3b66fb4
Osama Islam <osamasamarrai@gmail.com>
2022-01-28 19:31:07 +0200

    expand Blog schema

--- models/Blog.js
+++ models/Blog.js
@@ -1,5 +1,19 @@
 import mongoose from 'mongoose'
 
+const { ObjectId } = mongoose.SchemaTypes
+
+const commentSchema = new mongoose.Schema({
+	comment: {
+		type: String,
+		required: true
+	},
+	author: {
+		type: ObjectId,
+		ref: 'User',
+		required: true
+	}
+})
+
 const schema = new mongoose.Schema({
 	title: {
 		type: String,
@@ -11,10 +25,29 @@ const schema = new mongoose.Schema({
 		required: true
 	},
 	author: {
-		type: mongoose.SchemaTypes.ObjectId,
+		type: ObjectId,
 		ref: 'User',
 		required: true	
+	},
+	responseTo: {
+		type: ObjectId,
+		ref: 'Blog'
+	},
+	likes: [{
+		type: ObjectId,
+		ref: 'User',
+		default: []
+	}],
+	affirms: [{
+		type: ObjectId,
+		ref: 'User',
+		default: []
+	}],
+	comments: [commentSchema],
+	visible: {
+		type: Boolean,
+		default: false
 	}
-})
+}, { timestamps: true})
 
 export default mongoose.model('Blog', schema)
