4a895702fbd58739b3199a0fce9cade2abfd21b6
Osama Islam <osamasamarrai@gmail.com>
2022-01-29 18:30:34 +0200

    handling request just before it's sent

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ let app = express()
 import init from './init.js'
 import routes from './routes/index.js'
 
-
 init(app)
 
-
-
 app.use('/api', routes)
+
+app.use((pack, req, res, next) => {
+	let {
+		status,
+		data,
+		message
+	} = pack
+
+	res.status(status).json({data, message})
+})
 
 app.listen(process.env.PORT || 5000, () => 'Server is running...')
