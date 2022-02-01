9c3e5f99ba0ca5c92822cace9d7658ff096844d9
Osama Islam <osamasamarrai@gmail.com>
2022-02-01 16:12:10 +0200

    whitelist the frontend for cors

--- init.js
+++ init.js
@@ -4,13 +4,14 @@ import session from 'express-session'
 import passport from 'passport'
 import MongoStore from 'connect-mongo'
 import cors from 'cors'
+import strings from './lib/strings.js'
 
 import passportConfig from './lib/passport.js'
 
 export default app => {
 	app.use(express.json())
 	app.use(express.urlencoded({ extended: false}))
-	app.use(cors())
+	app.use(cors({credentials: true, origin: strings.clientAddress}))
 
 	db.connect()
 	const store = MongoStore.create({
