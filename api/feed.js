bc7118a644a70244471e35184ce1ee9fcabb654b
Osama Islam <osamasamarrai@gmail.com>
2022-02-01 16:14:25 +0200

    exclude the body from feed return

--- api/feed.js
+++ api/feed.js
@@ -4,9 +4,9 @@ import Blog from '../models/Blog.js'
 
 export default  async(req, res, next) => {
 	let data = await asyncHandler(
-		Blog.find({}).limit(10).lean()
+		Blog.find({}).select('-body').populate('author', 'username').limit(10).lean()
 	)
 	if(!data) return next(returnHandler(404, null, strings.noData))
-	if(data.error) return next(returnHandler(500, null, strings.SWR))
+	if(data.error) return next(returnHandler(500, data.error, strings.SWR))
 	return res.json(data)
 }
