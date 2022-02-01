2bbf1fe7713d894d673b756ae55bb82f8f0e64fb
Osama Islam <osamasamarrai@gmail.com>
2022-02-01 16:27:32 +0200

    adding more strings

--- client/src/lib/strings.js
+++ client/src/lib/strings.js
@@ -2,11 +2,20 @@ export default {
 	state: {
 		actions: {
 			auth: {
-				isAuthed: 'IS_AUTHED',
+				authenticate: 'AUTHENTICATE',
+				isAuthed: 'IS_AUTHENTICATED'
 			},
 			blog: {
 				getFeed: 'GET_FEED',
-				getBlog: 'GET_BLOG'
+				getBlog: 'GET_BLOG',
+				create: 'CREATE_BLOG',
+				update: 'UPDATE_BLOG',
+				delete: 'DELETE_BLOG',
+				like: 'LIKE',
+				unlike: 'UNLIKE',
+				affirm: 'AFFIRM',
+				unaffirm: 'UNAFFIRM',
+				comment: 'COMMENT',
 			},
 			getUser: 'GET_USER',
 		}
