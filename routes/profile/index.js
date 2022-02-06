a6bfaedc3b8adf327fe64edc38e1d843078d7836
Osama Islam <osamasamarrai@gmail.com>
2022-02-06 20:46:26 +0200

    fix the route

--- routes/profile/index.js
+++ routes/profile/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import getUser from '../../api/profile/profile.getUser.js'
 const rt = express.Router()
 
-rt.get('/:id', getUser)
+rt.get('/', getUser)
 
 
 export default rt
