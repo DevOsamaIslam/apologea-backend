3a700dc77026d043d34651ffbab30a3a944e2252
Osama Islam <osamasamarrai@gmail.com>
2022-02-06 20:43:52 +0200

    exclude the comments from the feed

--- api/feed.js
+++ api/feed.js
@@ -4,7 +4,7 @@ import Blog from '../models/Blog.js'
 
 export default  async(req, res, next) => {
 	let data = await asyncHandler(
-		Blog.find({}).select('-body').populate('author', 'username').limit(10).lean()
+		Blog.find({}).select('-body -comments').populate('author', 'username').limit(10).lean()
 	)
 	if(!data) return next(returnHandler(404, null, strings.noData))
 	if(data.error) return next(returnHandler(500, data.error, strings.SWR))
