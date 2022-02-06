59fbd02a8ef126e582a5a45dc3dabd5f8709dab6
Osama Islam <osamasamarrai@gmail.com>
2022-02-06 20:50:17 +0200

    adding edit and get blog service

--- client/src/controllers/services/blog.service.js
+++ client/src/controllers/services/blog.service.js
@@ -1,6 +1,7 @@
 import asyncHandler from '../../lib/utils'
 import blog from '../../state/actions/blog'
 import fetch from '../api/fetch'
+import blogActions from '../../state/actions/blog'
 
 let blogService = {}
 
@@ -17,6 +18,29 @@ blogService.create = async (e, form, dispatch, navigate) => {
 		navigate(`/blog/${data._id}`)	
 	}
 	
+}
+
+blogService.edit = async (e, form, dispatch, navigate) => {
+	e.preventDefault()
+	console.log('form', form)
+	let data = await asyncHandler(
+		fetch.updateBlog(form)
+	)
+	if(!data) console.log('No data returned')
+	else if(data.error) console.error(data.error)
+	else {
+		dispatch(blog.update(data))
+		navigate(`/blog/${data._id}`)	
+	}
+	
+}
+
+blogService.getBlog = async (id, dispatch) => {
+	let { data } = await asyncHandler(
+		fetch.getBlog(id)
+	)
+	dispatch(blogActions.view(data))
+	return data
 }
 
 export default blogService
