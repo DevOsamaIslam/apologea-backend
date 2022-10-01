00481d19686a1a225d3b29e230e65c16e84f8b00
Osama <Osamah.a@mblhightech.com>
2022-10-01 15:25:26 +0300

    try

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,7 +1,8 @@
-import { config } from 'dotenv'
+if (process.env.NODE_ENV !== 'production') {
+	console.log('development')
+	require('dotenv').config()
+} else console.log('production')
 
-config()
-
-export * from './app'
+export * from './settings'
 export * from './auth'
 export * from './messages'
