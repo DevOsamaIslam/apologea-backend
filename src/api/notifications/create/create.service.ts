3a8a684b5de429f093cd09e0225d5256c9ba120c
Osama <osamasamarrai@gmail.com>
2026-06-27 18:46:07 +0200

    return the notification documents after creation

--- src/api/notifications/create/create.service.ts
+++ src/api/notifications/create/create.service.ts
@@ -4,13 +4,12 @@ import { NotificationModel } from '../model/Notifications.Model'
 import { TCreateNotification } from '../notifications.schema'
 
 export const createNotificationService = async (notification: TCreateNotification) => {
-  return await asyncHandler(NotificationModel.create(notification))
+  return asyncHandler(NotificationModel.create(notification))
 }
 
 export const createMultipleNotificationsService = async (notifications: TCreateNotification[]) => {
-  NotificationModel.insertMany(notifications)
-
   notifications.forEach(notification => {
     SocketServer.sendToUser(notification.userId, notification.type, notification)
   })
+  return NotificationModel.insertMany(notifications)
 }
